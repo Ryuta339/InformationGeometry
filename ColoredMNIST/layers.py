1381869a3e7cd6295995f214697dcdd927a6c5d3
ShataKurashi <soccer.271828.339@gmail.com>
2020-11-24 23:31:25 +0900

    Added learning code in layers.py but something wrong

--- ColoredMNIST/layers.py
+++ ColoredMNIST/layers.py
@@ -1,8 +1,14 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+import torch.optim as optim
 
 ERMhidden_dim = 256
+
 
 class LayerBase(nn.Module):
     def __init__(self, input_dim: int, output_dim: int, hidden_dim: int):
@@ -24,23 +30,34 @@ class LayerBase(nn.Module):
     def forward(self, x):
         out = self._main(x)
         out = out.reshape(-1, self.output_dim)
-        m = nn.Softmax(dim=1)       # require?
-        return m(out)
+        return out
 
 
 class PhiLayer(LayerBase):
     def __init__(self, hidden_dim: int):
-        super(PhiLayer, self).__init__(28*28*2, 10, hidden_dim)
+        super(PhiLayer, self).__init__(28*28, 256, hidden_dim)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        return self.activation(super().forward(x))
 
 
 class FLayer(LayerBase):
     def __init__(self, hidden_dim: int):
-        super(FLayer, self).__init__(10, 10, hidden_dim)
+        super(FLayer, self).__init__(256, 10, hidden_dim)
+        self.activation = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        return self.activation(super().forward(x))
 
 
 class GLayer(LayerBase):
     def __init__(self, hidden_dim: int):
-        super(GLayer, self).__init__(10, 10, hidden_dim)
+        super(GLayer, self).__init__(256, 10, hidden_dim)
+        self.activation = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        return self.activation(super().forward(x))
 
 
 class Model(nn.Module):
@@ -49,17 +66,60 @@ class Model(nn.Module):
         self.phi = PhiLayer(hidden_dim)
         self.f = FLayer(hidden_dim)
         self.g = GLayer(hidden_dim)
-        
-    def forward(self, x):
-        x = self.phi.forward(x)
+
+    def forward(self, x0):
+        x = self.phi.forward(x0)
         y = self.f.forward(x)
         z = self.g.forward(x)
-        return torch.cat([y,z], axis=0)
+        return torch.cat([y, z], axis=0)
+
+
+# obtain MNIST
+transform = transforms.Compose([transforms.ToTensor()])
+trainset = torchvision.datasets.MNIST(root='./data',
+                                      train=True,
+                                      download=True,
+                                      transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=100,
+                                          shuffle=True,
+                                          num_workers=2)
+testset = torchvision.datasets.MNIST(root='./data',
+                                     train=False,
+                                     download=True,
+                                     transform=transform)
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size=100,
+                                         shuffle=False,
+                                         num_workers=2)
+classes = tuple(np.linspace(0, 9, 10, dtype=np.uint8))
 
 model = Model(ERMhidden_dim)
-print (model)
 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-sample_input = torch.randn(1,28*28*2)
-outputs = model(sample_input)
-print (outputs)
+epochs = 20
+
+for epoch in range(epochs):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(trainloader, 0):
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward
+        outputs = model(inputs)
+        loss = criterion(outputs, torch.cat([labels, labels], axis=0))
+        # backward
+        loss.backward()
+        # optimize
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[{:d}, {:5d}] loss: {:.3f}'
+                  .format(epoch+1, i+1, running_loss/100))
+            running_loss = 0.0
+
+print('Finished Training')
